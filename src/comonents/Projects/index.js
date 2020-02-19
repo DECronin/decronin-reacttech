4f49330442841516bba29183a0f5d73b9cbfa436
Dana Cronin <cronin43224235@gmail.com>
2020-02-19 16:11:30 -0500

    moving portfolio items array and slider

--- src/comonents/Projects/index.js
+++ src/comonents/Projects/index.js
@@ -1,44 +1,127 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
+import {FaAngleDoubleRight, FaAngleDoubleLeft} from "react-icons/fa";
 
-function Projects(){
+const portfolio = [{
+  title: "Empty Kitchen",
+  imgSrc: "./files/food.jpg",
+  imgAlt: "Rutgers Info Widget",
+  url: "https://decronin.github.io/project-one-ddb/"
+}, {
+  title: "Cypher-Text",
+  imgSrc: "./files/firewall-994x400.jpg",
+  igAlt: "cypher-text",
+  url: "https://cypher-text.herokuapp.com/"
+}, {
+  title: "Word Guess",
+  imgSrc: "./files/1_3FTNbYQ3pWzJ1u4XF1Suzw.png",
+  imgAlt: "Word Guess",
+  url: "https://decronin.github.io/Word-Guess-Game/"
+}, {
+  title: "Bamazon",
+  imgSrc: "./files/Amazon-Smile-Squ-f9a6e959-ed0d-466b-920a-e9a214e54c02._CB514185896_.jpg",
+  imgAlt: "bamazon",
+  url: "https://github.com/DECronin/bamazon"
+}, {
+  title: "Gif Searcher",
+  imgSrc: "./files/searching.jpg",
+  imgAlt: "Giphy",
+  url: "https://decronin.github.io/Gif-Searcher/"
+}, {
+  title: "Train Calculator",
+  imgSrc: "./files/technics-q-c-640-480-9.jpg",
+  imgAlt: "Train Scheduler",
+  url: "https://decronin.github.io/TrainScheduleCalculator/"
+}]
 
-    return(<>
-        <div class="container portfolio-list" id="content-center">
-          <div class="row">
-            <h2 id="portfolio-h2">Portfolio</h2>
-          </div>
-          <div class="work"> <a href="https://decronin.github.io/project-one-ddb/" target="_blank">
-            <img src="./files/food.jpg" alt="Rutgers Info Widget" />
-            <h3>Empty Kitchen</h3></a>
-          </div>
-          <div class="work"> <a href="https://cypher-text.herokuapp.com/" target="_blank">
-            <img src="./files/firewall-994x400.jpg" alt="cypher-text" />
-    
-            <h3>Cypher-Text</h3></a>
-          </div>
-          <div class="work"><a href="https://decronin.github.io/Word-Guess-Game/" target="_blank">
-            <img src="./files/1_3FTNbYQ3pWzJ1u4XF1Suzw.png" alt="Word Guess" />
-    
-            <h3>Word Guess</h3></a>
-          </div>
-          <div class="work"> <a href="https://github.com/DECronin/bamazon" target="_blank">
-            <img src="./files/Amazon-Smile-Squ-f9a6e959-ed0d-466b-920a-e9a214e54c02._CB514185896_.jpg" alt="bamazon" />
-    
-            <h3>Bamazon</h3></a>
-          </div>
-          <div class="work"> <a href="https://decronin.github.io/Gif-Searcher/" target="_blank">
-            <img src="./files/searching.jpg" />
-            <h3>Gif Searcher</h3></a>
-          </div>
-          <div class="work"> <a href="https://decronin.github.io/TrainScheduleCalculator/" target="_blank">
-            <img src="./files/technics-q-c-640-480-9.jpg" alt="Rock Paper Scissors" />
-    
-            <h3>Train Calculator</h3> </a>
-          </div>
-        </div>
-    </>)
+function Projects() {
 
+  const [state, setThisState] = useState({
+    displayingIndex: 0,
+    currentCard: <><p>Loading</p></>
+  })
+  // When the component mounts, a call will be made to get random users.
+  useEffect(() => {
+    buildDisplayFocus();
+  }, [state.displayingIndex]);
+
+  function nextUser(userIndex) {
+    // Ensure that the user index stays within our range of users
+    if (userIndex >= portfolio.length) {
+      userIndex = 0;
+    }
+    // setUser(users[userIndex]);
+    setThisState({...state,
+      displayingIndex: userIndex
+    })
+  }
+
+  function previousUser(userIndex) {
+    // Ensure that the user index stays within our range of users
+    if (userIndex < 0) {
+      userIndex = portfolio.length - 1;
+    }
+    // setUser(users[userIndex]);
+    setThisState({...state,
+      displayingIndex: userIndex
+    })
+  }
+
+  function handleBtnClick(btnName) {
+    if (btnName === "next") {
+      const newUserIndex = state.displayingIndex + 1;
+      nextUser(newUserIndex);
+    } else {
+      const newUserIndex = state.displayingIndex - 1;
+      previousUser(newUserIndex);
+    }
+  }
+
+
+  function buildDisplayFocus(){
+    console.log(`line 84?`)
+    setThisState({
+      ...state, currentCard:
+        <>
+          <div className="row">
+            <button className="col-3 btn" onClick={() => handleBtnClick("back")}>
+              <FaAngleDoubleLeft
+                color="#008f68"
+                size="50px"
+                style={{ margin: '0 5px' }} />
+            </button>
+            <div className="col-6">
+
+              <a href={portfolio[state.displayingIndex].url}>
+                <img src={portfolio[state.displayingIndex].imgSrc} alt={portfolio[state.displayingIndex].imgAlt} className="work" />
+                <h2>{portfolio[state.displayingIndex].title}</h2>
+              </a>
+
+            </div>
+              <button className="col-3 btn" onClick={() => handleBtnClick("next")}>
+                <FaAngleDoubleRight
+                  color="#008f68"
+                  size="50px"
+                  style={{ margin: '0 5px' }} />
+              </button>
+          </div>
+        </>
+    })
+  }
+
+  return (<>
+    <div class="container portfolio-list" id="content-center">
+      <div class="row">
+        <h2 id="portfolio-h2">Portfolio</h2>
+      </div>
+      {state.currentCard}
+      {/* <div class="work"> <a href="https://github.com/DECronin/bamazon" target="_blank">
+        <img src="./files/Amazon-Smile-Squ-f9a6e959-ed0d-466b-920a-e9a214e54c02._CB514185896_.jpg" alt="bamazon" />
+
+        <h3>Bamazon</h3></a>
+      </div> */}
+    </div>
+  </>)
 }
 
 export default Projects
