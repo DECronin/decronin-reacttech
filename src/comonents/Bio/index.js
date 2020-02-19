5fd6d2db9e03cbe521b99cb81e50b81aa52e71ac
Dana Cronin <cronin43224235@gmail.com>
2020-02-19 16:09:35 -0500

    moving jumbotron to header

--- src/comonents/Bio/index.js
+++ src/comonents/Bio/index.js
@@ -6,12 +6,6 @@ function Bio() {
         <div class="container">
 
             <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4">Dana Cronin</h1>
-                    <img src="./files/ny-selfy.jpg" alt="my-face" id="bio-pic" />
-                    <p class="lead">"There Are Three Types of People: Those Who Make Things Happen, Those Who Watch Things Happen, and Those Who Wonder What Happened."</p>
-                    <p class="quote-source">--Nicholas Murray Butler</p>
-                </div>
                 <div class="container" id="content-center">
                     <div class="row">
                         <h2>About Me</h2>
