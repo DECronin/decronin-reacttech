ad56e76f5163ee9dcf6955d3cda45708cbfadf64
Dana Cronin <cronin43224235@gmail.com>
2020-02-28 14:31:57 -0500

    badges for logos prep

--- src/comonents/Logo/index.js
+++ src/comonents/Logo/index.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 function Logo(){
     return(<>
-        <h1>DECronin</h1>
-        <h2>*LOGO* Coming Soon</h2>
+        <h1>Technologies Badges:</h1>
+        <div className="logo-tech-links">
+            <a href="">html</a>
+            <a href="">css</a>
+            <a href="">js</a>
+            <a href="">node</a>
+            <a href="">react</a>
+            <a href="">mysql</a>
+            <a href="">mongo</a>
+            <a href="">handlebars</a>
+            <a href="">express</a>
+            <a href="">botstrap</a>
+            <a href="">materialize</a>
+            <a href="">paint tool sai</a>
+            <a href="">gimp</a>
+            <a href="">java</a>
+            <a href="">heroku</a>
+            <a href="">git</a>
+            <a href="">json</a>
+            <a href="">firebase</a>
+        </div>
     </>)
 }
 
