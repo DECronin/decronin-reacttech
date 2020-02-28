8b5272a73e764b0d55a0f6311da9d4404f30c0f1
Dana Cronin <cronin43224235@gmail.com>
2020-02-28 14:04:44 -0500

    adding claddes for style elements

--- src/comonents/Header/index.js
+++ src/comonents/Header/index.js
@@ -9,10 +9,10 @@ return (<>
         <div className="col-3">
             <img src="./files/ny-selfy.jpg" alt="my-face" id="bio-pic" />
         </div>
-        <div className="col-2">
+        <div className="col-2 h-name">
             <h1 class="display-4">Dana Cronin</h1>
         </div>
-        <div className="col-4">
+        <div className="col-4 h-quote">
             <p class="lead">"There Are Three Types of People: Those Who Make Things Happen, Those Who Watch Things Happen, and Those Who Wonder What Happened."</p>
             <p class="quote-source">--Nicholas Murray Butler</p>
         </div>
