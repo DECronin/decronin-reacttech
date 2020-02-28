89e71162fb5d817283eb7e5b42509baf84d38799
Dana Cronin <cronin43224235@gmail.com>
2020-02-28 17:46:28 -0500

    updating branding statement

--- src/comonents/Bio/index.js
+++ src/comonents/Bio/index.js
@@ -11,9 +11,9 @@ function Bio() {
                         <h2>About Me</h2>
                     </div>
                     <div id="bio-text">
-                        <p>Multidisciplinary Solutions Analyst with Certificate in Full Stack Web Development accompanying a background in Mathematics, Graphic Design, and Business Development. Outstanding problem-solving skills with a passion for generating work that exceeds the expectations of the client.</p>
-                        <p>Adept from prototype to implementation; ability to rally self and/or team through comprehensively remaining focused on the client’s mission. Critical eye for details from converting stale code to debugging or condensing into production.</p>
-                        <p>Positioned well to provide unique perspectives on end-user interactions for websites’ flow and logic by leveraging expertise in liaison organization communications by applying elasticity to fresh concepts.</p>
+                        <p>Multidisciplinary Solutions Analyst with Certificate in Full Stack Web Development accompanying a background in Mathematics, Graphic Design, and Business Development. Outstanding problem-solving skills with a passion for generating work that exceeds the expectations of the client</p>
+                        <p>Adept from prototype to implementation; ability to rally self and/or team through comprehensively remaining focused on the client’s mission. Critical eye for details from converting stale code to debugging or condensing into production. Proficiency and continued interest in JavaScript, CSS, and HTML (as well as HTML interfacings such as React.js and Handlebars).</p>
+                        <p>Brainstorming Wireframes comes second nature with JavaScript functionality applying to HTML compartmentalization experience. Positioned well to provide unique perspectives on end-user interactions for websites’ flow and logic by leveraging expertise in liaison organization communications by applying elasticity to fresh concepts.</p>
                     </div>
                 </div>
             </div>
