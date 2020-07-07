f963f712169d527a383840577de88ce0a779357a
Dana Cronin <cronin43224235@gmail.com>
2020-07-07 10:15:26 -0500

    fixing to className

--- src/comonents/Header/index.js
+++ src/comonents/Header/index.js
@@ -10,11 +10,11 @@ return (<>
             <img src="./files/0sm-dec.jpg" alt="my-face" id="bio-pic" />
         </div>
         <div className="col-2 h-name">
-            <h1 class="display-4">Dana Cronin</h1>
+            <h1 className="display-4">Dana Cronin</h1>
         </div>
         <div className="col-4 h-quote">
-            <p class="lead">"There Are Three Types of People: Those Who Make Things Happen, Those Who Watch Things Happen, and Those Who Wonder What Happened."</p>
-            <p class="quote-source">--Nicholas Murray Butler</p>
+            <p className="lead">"There Are Three Types of People: Those Who Make Things Happen, Those Who Watch Things Happen, and Those Who Wonder What Happened."</p>
+            <p className="quote-source">--Nicholas Murray Butler</p>
         </div>
         <div className="col-1"></div>
     </header>
