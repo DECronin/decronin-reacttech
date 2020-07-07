c27ea98be8f285018666bbb2a474aa00ba3e5aee
Dana Cronin <cronin43224235@gmail.com>
2020-07-07 13:00:31 -0500

    header sections/col respnsive

--- src/comonents/Header/index.js
+++ src/comonents/Header/index.js
@@ -5,18 +5,18 @@ function Header() {
 return (<>
     <Nav />
     <header className="row justify-content-center">
-        <div className="col-2"></div>
-        <div className="col-3">
+        <div className="col-lg-2"></div>
+        <div className="col-lg-3 col-md-6">
             <img src="./files/0sm-dec.jpg" alt="my-face" id="bio-pic" />
         </div>
-        <div className="col-2 h-name">
+        <div className="col-lg-2 col-md-6 h-name">
             <h1 className="display-4">Dana Cronin</h1>
         </div>
-        <div className="col-4 h-quote">
+        <div className="col-lg-4 col-md-12 h-quote">
             <p className="lead">"There Are Three Types of People: Those Who Make Things Happen, Those Who Watch Things Happen, and Those Who Wonder What Happened."</p>
             <p className="quote-source">--Nicholas Murray Butler</p>
         </div>
-        <div className="col-1"></div>
+        <div className="col-lg-1"></div>
     </header>
 </>)
 }
